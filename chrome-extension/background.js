2dece1ae6db49cf42be47686601449362ae5b8d4
Bill Stavroulakis <bstavroulakis@mesosphere.io>
2017-09-29 14:42:57 -0700

    multiple tab, cloud storage

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,87 +1,30 @@
+var toggleState = false;
+var storageKey = "dcos-dark-theme-state";
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  toggle = !getState()
- // var key = "myKey",
- //     testPrefs = JSON.stringify({
- //         'val': 10
- //     });
- // var jsonfile = {};
- // jsonfile[key] = testPrefs;
- // chrome.storage.sync.set(jsonfile, function () {
- //     console.log('Saved', key, testPrefs);
- //  });
- // }
-
-  if (toggle) {
-    chrome.browserAction.setIcon({ path: "icons/on.png", tabId: tab.id });
-    chrome.tabs.sendMessage(tab.id, { dark: true }, function(response) {
-      // no-op
-    });
-  } else {
-    chrome.browserAction.setIcon({ path: "icons/off.png", tabId: tab.id });
-    chrome.tabs.sendMessage(tab.id, { dark: false }, function(response) {
-      // no-op
-    });
-  }
-  saveChanges(toggle);
+chrome.storage.sync.get(storageKey, function(obj) {
+  toggleState = obj[storageKey];
+  refreshIcon();
 });
-
-chrome.tabs.onActivated.addListener(function(tab) {
-  if (toggle) {
-    chrome.browserAction.setIcon({ path: "icons/on.png", tabId: tab.id });
-  } else {
-    chrome.browserAction.setIcon({ path: "icons/off.png", tabId: tab.id });
-  }
-});
-
-function getState() {
-    chrome.storage.sync.get('dcos-dark-theme-state', function (obj) {
-        // Initialize state if not initialized
-	return obj;
-    });
-}
-
-function saveChanges(toggleState) {
-  // Save it using the Chrome extension storage API.
-  chrome.storage.sync.set({'dcos-dark-theme-state': toggleState}, function() {
-    // Notify that we saved.
-    message('Settings saved');
-  });
-}
 
 chrome.storage.onChanged.addListener(function(changes, namespace) {
-    for (key in changes) {
-      var storageChange = changes[key];
-      console.log('Storage key "%s" in namespace "%s" changed. ' +
-                  'Old value was "%s", new value is "%s".',
-                  key,
-                  namespace,
-                  storageChange.oldValue,
-                  storageChange.newValue);
+  for (key in changes) {
+    var storageChange = changes[key];
+    if (key === storageKey) {
+      if (toggleState !== storageChange.newValue) {
+        refreshIcon();
+      }
+      toggleState = storageChange.newValue;
+      break;
     }
-  });
-      
+  }
+});
 
-$(function(){
-    //default value is "start"
-    var currentState = localStorage.currentState || "start";
-    //cache button DOM element reference
-    var $toggleBtn = $("#toggle-btn");
+var refreshIcon = function() {
+  var image = toggleState ? "on" : "off";
+  chrome.browserAction.setIcon({ path: "icons/" + image + ".png" });
+};
 
-    //update button status
-    if(currentState==="stop"){
-        $toggleBtn.text("OFF");
-    }
-
-    //register button click handler
-    $toggleBtn.click(function(){
-        if(currentState==="start"){
-            $toggleBtn.text("OFF");
-            localStorage.currentState="stop";
-        }
-        if(currentState==="stop"){
-            $toggleBtn.text("ON");
-            localStorage.currentState="start";
-        }
-    });
+chrome.browserAction.onClicked.addListener(function(tab) {
+  var toggle = !toggleState;
+  chrome.storage.sync.set({ [storageKey]: toggle });
 });
--- chrome-extension/style.js
+++ chrome-extension/style.js
@@ -1,14 +1,43 @@
-// insert our override styles
-var style = document.createElement('link');
-style.rel = 'stylesheet';
-style.type = 'text/css';
-style.href = chrome.extension.getURL('styles/dist/styles.css');
-(document.head||document.documentElement).appendChild(style);
+var toggleState = false;
+var storageKey = "dcos-dark-theme-state";
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.dark) {
-        document.body.className = "dark";
-    } else {
-        document.body.className = "";
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  for (key in changes) {
+    var storageChange = changes[key];
+    if (key === storageKey) {
+      if (toggleState !== storageChange.newValue) {
+        refreshStyles();
+      }
+      toggleState = storageChange.newValue;
+      break;
     }
+  }
+});
+
+var refreshStyles = function() {
+  if (!document.querySelector("#dcos-dark-style")) {
+    addStylesOnPage();
+  }
+  if (toggleState) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+};
+
+var addStylesOnPage = function() {
+  var style = document.createElement("link");
+  style.rel = "stylesheet";
+  style.type = "text/css";
+  style.id = "dcos-dark-style";
+  style.href = chrome.extension.getURL("styles/dist/styles.css");
+  (document.head || document.documentElement).appendChild(style);
+};
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  chrome.storage.sync.get(storageKey, function(obj) {
+    toggleState = obj[storageKey];
+    console.log(toggleState);
+    refreshStyles();
+  });
 });
