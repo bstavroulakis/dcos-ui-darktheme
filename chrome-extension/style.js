355b53960f7128f24c662e0d7e1524074c96791e
Bill Stavroulakis <bstavroulakis@mesosphere.io>
2017-10-03 19:55:54 -0700

    fix(styles.js): add external stylesheet
    Add external stylesheet source

--- chrome-extension/style.js
+++ chrome-extension/style.js
@@ -16,7 +16,7 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
 
 var refreshStyles = function() {
   if (!isDCOS || !document.body) {
-    removeStylesFromPage();  
+    removeStylesFromPage();
     return;
   }
 
@@ -30,28 +30,38 @@ var refreshStyles = function() {
 
 var changeIcons = function() {
   if (toggleState) {
-      // add inverse
-      Array.prototype.slice.call(document.getElementsByTagName("use")).forEach(function(use) {
-          if (use.href.baseVal.indexOf("inverse") === -1 && use.href.baseVal.indexOf("product") > -1) {
-              use.href.baseVal += "-inverse";
-          }
+    // add inverse
+    Array.prototype.slice
+      .call(document.getElementsByTagName("use"))
+      .forEach(function(use) {
+        if (
+          use.href.baseVal.indexOf("inverse") === -1 &&
+          use.href.baseVal.indexOf("product") > -1
+        ) {
+          use.href.baseVal += "-inverse";
+        }
       });
-      return;
+    return;
   }
 
-  Array.prototype.slice.call(document.getElementsByTagName("use")).forEach(function(use) {
-    if (use.href.baseVal.indexOf("inverse") > -1 && use.href.baseVal.indexOf("product") > -1) {
+  Array.prototype.slice
+    .call(document.getElementsByTagName("use"))
+    .forEach(function(use) {
+      if (
+        use.href.baseVal.indexOf("inverse") > -1 &&
+        use.href.baseVal.indexOf("product") > -1
+      ) {
         if (use.parentNode.parentNode.nodeName === "A") {
-            return;
+          return;
         }
         use.href.baseVal = use.href.baseVal.replace(/-inverse/g, "");
-    }
-  });
+      }
+    });
 };
 
 var addStylesOnPage = function() {
   if (!isDCOS || !toggleState) {
-    removeStylesFromPage(); 
+    removeStylesFromPage();
     return;
   }
   var overrideSheet = document.getElementById(storageKey);
@@ -62,23 +72,24 @@ var addStylesOnPage = function() {
   style.rel = "stylesheet";
   style.type = "text/css";
   style.id = storageKey;
-  style.href = chrome.extension.getURL("styles/dist/styles.css");
+  style.href =
+    "https://rawgit.com/bstavroulakis/dcos-ui-darktheme/master/chrome-extension/styles/dist/styles.css";
   (document.head || document.documentElement).appendChild(style);
 };
 
 var removeStylesFromPage = function() {
-    var overrideSheet = document.getElementById(storageKey);
-    if (overrideSheet) {
-        overrideSheet.parentNode.removeChild(overrideSheet);        
-    }
+  var overrideSheet = document.getElementById(storageKey);
+  if (overrideSheet) {
+    overrideSheet.parentNode.removeChild(overrideSheet);
+  }
 };
 
 function dcosStateLoaded() {
-  if(!isDCOS){
+  if (!isDCOS) {
     return;
   }
 
-  setInterval(function(){
+  setInterval(function() {
     changeIcons();
   }, 100);
 
@@ -91,21 +102,21 @@ function dcosStateLoaded() {
 }
 
 function dcosMetadataListener() {
-  try{
+  try {
     var dcosMetadata = JSON.parse(this.responseText);
     if (!dcosMetadata || !this.responseText.includes("dcos-image-commit")) {
       isDCOS = false;
-    }else{
+    } else {
       isDCOS = true;
     }
-  }catch(ex){
+  } catch (ex) {
     isDCOS = false;
   }
   dcosStateLoaded();
 }
 
-setTimeout(function(){
-  if(document && document.title && document.title.includes("DC/OS")) {
+setTimeout(function() {
+  if (document && document.title && document.title.includes("DC/OS")) {
     oReq.addEventListener("load", dcosMetadataListener);
     oReq.open("GET", "/dcos-metadata/dcos-version.json");
     oReq.send();
